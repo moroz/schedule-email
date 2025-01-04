000236ac4b7161bbe07db0560be3168c5f050d6e
Karol Moroz <karol@moroz.dev>
2025-01-04 15:54:50 +0800

    Install imap client

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/moroz/schedule-email
 
 go 1.23.4
+
+require (
+	github.com/emersion/go-imap/v2 v2.0.0-beta.4 // indirect
+	github.com/emersion/go-message v0.18.1 // indirect
+	github.com/emersion/go-sasl v0.0.0-20241020182733-b788ff22d5a6 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/emersion/go-imap/v2/imapclient"
+)
+
+func MustGetenv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("FATAL: Environment variable %s is not set!", key)
+	}
+	return value
+}
+
+var IMAP_SERVER = MustGetenv("IMAP_SERVER")
+var IMAP_USER = MustGetenv("IMAP_USER")
+var IMAP_PASSWORD = MustGetenv("IMAP_PASSWORD")
 
 func main() {
-	fmt.Println("Hello, world!")
+	client, err := imapclient.DialTLS(IMAP_SERVER+":993", &imapclient.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = client
+
+	cmd := client.Login(IMAP_USER, IMAP_PASSWORD)
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
